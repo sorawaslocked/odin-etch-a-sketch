6b54c8e7bec61382794bbd406d937c225cfdaff7
sora <133684349+sorawaslocked@users.noreply.github.com>
2024-01-08 18:31:49 +0600

    Rework grid creation
    Past method of creating grid was leaving empty spaces between squares
    and the outer border with the gridsize of 50 or higher.
    New method creates rows of squares without the need of flex-wrap
    property.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const changeGridSizeBtn = document.querySelector('#changeGridSizeBtn');
 // Initializing grid
 let gridSize = 16;
 let squareArray = createSquareArray(gridSize);
+let color = 'black';
 initBoard(squareArray);
 
 changeGridSizeBtn.addEventListener('click', () => {
@@ -26,16 +27,21 @@ function clearBoard() {
   }
 }
 
+// Array is created from rows of squares
 function createSquareArray(gridWidth) {
   let squareArray = []
   
-  for (let i = 0; i < gridWidth * gridWidth; i++) {
-    const square = document.createElement('div');
-    square.style.width = `${MAIN_BOARD_WIDTH / gridWidth}px`;
-    square.style.height = `${MAIN_BOARD_WIDTH / gridWidth}px`;
-    square.style.boxSizing = 'border-box';
-    square.style.border = '1px solid black';
-    squareArray.push(square);
+  for (let i = 0; i < gridWidth; i++) {
+    let squareRow = document.createElement('div')
+    for (let j = 0; j < gridWidth; j++) {
+      const square = document.createElement('div');
+      square.style.width = `${MAIN_BOARD_WIDTH / gridWidth}px`;
+      square.style.height = `${MAIN_BOARD_WIDTH / gridWidth}px`;
+      square.style.boxSizing = 'border-box';
+      square.style.border = '1px solid black';
+      squareRow.appendChild(square);
+    }
+    squareArray.push(squareRow);
   }
 
   return squareArray;
@@ -43,7 +49,7 @@ function createSquareArray(gridWidth) {
 
 function handleMouseInput(squareArray) {
   document.body.onmousedown = () => {
-    addSquareHovering(squareArray, 'black');
+    addSquareHovering(squareArray, color);
   }
   document.body.onmouseup = () => {
     removeSquareHovering(squareArray);
@@ -51,32 +57,41 @@ function handleMouseInput(squareArray) {
 }
 
 function disableDrag(squareArray) {
-  squareArray.forEach((square) => {
-    square.addEventListener('dragstart', event => {
-      event.preventDefault();
-    });
-    square.addEventListener('drop', event => {
-      event.preventDefault();
+  squareArray.forEach((squareRow) => {
+    let squares = squareRow.querySelectorAll('div');
+    squares.forEach((square) => {
+      square.addEventListener('dragstart', event => {
+        event.preventDefault();
+      });
+      square.addEventListener('drop', event => {
+        event.preventDefault();
+      });
     });
   });
 }
 
 function addSquareHovering(squareArray, color) {
-  squareArray.forEach((square) => {
-    square.onmouseenter = () => {
-      square.style.backgroundColor = color;
-    }
+  squareArray.forEach((squareRow) => {
+    let squares = squareRow.querySelectorAll('div');
+    squares.forEach((square) => {
+      square.onmouseenter = () => {
+        square.style.backgroundColor = color;
+      }
+    });
   });
 }
 
 function removeSquareHovering(squareArray) {
-  squareArray.forEach((square) => {
-    square.onmouseenter = null;
+  squareArray.forEach((squareRow) => {
+    let squares = squareRow.querySelectorAll('div');
+    squares.forEach((square) => {
+      square.onmouseenter = null;
+    });
   });
 }
 
 function fillMainBoard(squareArray) {
-  squareArray.forEach((square) => {
-    board.appendChild(square);
+  squareArray.forEach((squareRow) => {
+    board.appendChild(squareRow);
   });
 }
