ad523e69a689f810b9ad51fc6ad51f129874e242
sora <133684349+sorawaslocked@users.noreply.github.com>
2024-01-08 16:27:04 +0600

    Add mouse hover coloring function

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const board = document.querySelector('#mainBoard');
 
 // MAIN CODE
 let squareArray = createSquareArray(16);
-fillMainBoard(16, squareArray);
+addSquareHovering(squareArray, 'red');
+fillMainBoard(squareArray);
 
 function createSquareArray(gridWidth) {
   let squareArray = []
   
   for (let i = 0; i < gridWidth * gridWidth; i++) {
     const square = document.createElement('div');
-    square.style.cssText = `width: ${MAIN_BOARD_WIDTH / gridWidth}px; height: ${MAIN_BOARD_WIDTH / gridWidth}px`;
+    square.style.width = `${MAIN_BOARD_WIDTH / gridWidth}px`;
+    square.style.height = `${MAIN_BOARD_WIDTH / gridWidth}px`;
     square.style.boxSizing = 'border-box';
     square.style.border = '1px solid black';
     squareArray.push(square);
@@ -19,7 +21,16 @@ function createSquareArray(gridWidth) {
   return squareArray;
 }
 
-function fillMainBoard(gridWidth, squareArray) {
-  for (let i = 0; i < gridWidth * gridWidth; i++)
-    board.appendChild(squareArray[i]);
-} 
+function addSquareHovering(squareArray, color) {
+  squareArray.forEach((square) => {
+    square.addEventListener('mouseenter', () => {
+      square.style.backgroundColor = color;
+    })
+  });
+}
+
+function fillMainBoard(squareArray) {
+  squareArray.forEach((square) => {
+    board.appendChild(square);
+  });
+}
